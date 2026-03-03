73946fd6eb85c1f13fb9d57164d6a20b71a7cc73
Mehulanand-bit <230276.ee@rmkec.ac.in>
2026-03-03 21:36:40 +0530

    Update extending_thread.java

--- extending_thread.java
+++ extending_thread.java
@@ -14,3 +14,19 @@ public class Worker extends Thread {
         }
     }
 }
+
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        for(int wid = 1; wid <= K; wid++){
+            Worker w = new Worker(wid);
+            w.doWork(N);
+        }
+
+    }
+}
